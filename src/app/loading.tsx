5481c44bef57fc5b603664abfa1b8c074d18c761
Arjun-Singh-123 <arjun@appteg.com>
2024-09-13 14:47:40 +0530

    root layout loader updated

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,16 +1,20 @@
 // src/components/Loading.tsx
+import Loader from "@/components/common/loader";
 import React from "react";
 
 const Loading: React.FC = () => {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <img
-        src="https://dev.windwardsailingclub.com/img/loading.svg"
-        alt="Loading..."
-        className="w-46 h-46"
-      />
-    </div>
-  );
+  return <Loader />;
+
+  // return (
+
+  //   // <div className="flex items-center justify-center h-screen">
+  //   //   <img
+  //   //     src="https://dev.windwardsailingclub.com/img/loading.svg"
+  //   //     alt="Loading..."
+  //   //     className="w-46 h-46"
+  //   //   />
+  //   // </div>
+  // );
 };
 
 export default Loading;
