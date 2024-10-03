777d102eaf85548614e15496af0a4dde6c4bfdbb
Senjalkumar <160628356+Senjalkumar@users.noreply.github.com>
2024-10-03 18:22:47 +0530

    member page css changes of half circle and content updated

--- src/components/common/member-card.tsx
+++ src/components/common/member-card.tsx
@@ -18,35 +18,42 @@ const MemberCard: React.FC<MemberProps> = ({
   email,
   phone,
 }) => (
-  <Card className="relative border-2 pt-12 border-t-blue-500 mb-12 min-h-[300px]">
+  <Card className="relative border-t-[0.625rem] pt-12 border-t-blue-500 mb-12 min-h-[300px]">
     <div className="absolute -top-12 left-1/2 transform -translate-x-1/2">
-      <div className="w-24 h-24 rounded-full overflow-hidden border-b-8 border-b-blue-500">
+      <div className="w-44 h-44 rounded-full overflow-hidden border-[0.625rem] border-r-blue-500 border-b-blue-500 transform rotate-45 -mt-[3.125rem] ">
         <Image
           src={image}
           alt={name}
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover -rotate-45"
           width={96}
           height={96}
         />
       </div>
     </div>
-    <CardContent>
-      <h2 className="text-2xl font-semibold text-center mb-2">{name}</h2>
-      <p className="text-sm mb-4">{about}</p>
-      <div className="flex items-center justify-center space-x-4">
+    <CardContent className="flex flex-col items-center text-center mt-10">
+      <h2 className="text-2xl font-semibold mb-1">Amanda Martin</h2>
+      <h4 className="text-lg font-medium text-muted-foreground mb-4">
+        SAFETY OFFICER
+      </h4>
+      <p className="text-sm mb-6 px-4">
+        <span className="font-semibold">About: </span>
+        Ensures adherence to safety protocols and conducts safety briefings for
+        members.
+      </p>
+      <div className="flex flex-col gap-2 items-start w-full px-4">
         <a
-          href={`mailto:${email}`}
-          className="flex items-center text-blue-500 hover:underline"
+          href="mailto:amanda.martin@example.com"
+          className="flex items-center text-primary hover:underline"
         >
-          <Mail className="w-4 h-4 mr-1" />
-          <span className="text-sm">{email}</span>
+          <Mail className="w-4 h-4 mr-2 flex-shrink-0" />
+          <span className="text-sm">Email: amanda.martin@example.com</span>
         </a>
         <a
-          href={`tel:${phone}`}
-          className="flex items-center text-blue-500 hover:underline"
+          href="tel:(555) 234-5678"
+          className="flex items-center text-primary hover:underline"
         >
-          <Phone className="w-4 h-4 mr-1" />
-          <span className="text-sm">{phone}</span>
+          <Phone className="w-4 h-4 mr-2 flex-shrink-0" />
+          <span className="text-sm">Phone: (555) 234-5678</span>
         </a>
       </div>
     </CardContent>
